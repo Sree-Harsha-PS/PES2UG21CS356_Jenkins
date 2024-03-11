13c7db32a5186c7e1884126e0029937de58c17f1
P S Sree Harsha <sreeharshapolepalli@gmail.com>
2024-03-12 01:20:58 +0530

    Jenkins file

---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Main World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run the main file\n";
+  cout << "Its running by PES2UG21CS356 YEHH";
+  return 0;
+}
