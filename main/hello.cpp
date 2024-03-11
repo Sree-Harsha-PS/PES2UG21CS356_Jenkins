bcaac2f42ac5089608e8e6cd67c2c08a5f98cc6f
Sree Harsha <95706697+Sree-Harsha-PS@users.noreply.github.com>
2024-03-12 00:37:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Its running by PES2UG21CS356 YEHH";
+  cout << "Its running by PES2UG21CS356 YEHH\n";
   return 0;
 }
